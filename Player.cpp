88ecb6443c7763c2e691feae9fddcb0fd79c81a0
鏡　桂一郎 <k022G0120@g.neec.ac.jp>
2023-11-01 12:16:05 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.cpp
@@ -0,0 +1,24 @@
+﻿#include "Player.h"
+#include "ImGuiManager.h"
+#include "Vector3.h"
+#include <cassert>
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	// NULLポインタテェック
+	assert(model);
+
+	// メンバ変数に受け取った値を代入
+	textureHandle_ = textureHandle;
+	model_ = model;
+
+	// ワールド初期化
+	worldTransform_.Initialize();
+}
+
+void Player::Update() {
+	// WorldTransform_.TransferMatr
+}
+
+void Player::Draw(ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
---
+++ scene/GameScene.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "Audio.h"
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "Model.h"
+#include "Player.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+public: // メンバ関数
+	/// <summary>
+	/// コンストクラタ
+	/// </summary>
+	GameScene();
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+private: // メンバ変数
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+
+	// ビュープロテクション
+	ViewProjection viewProjection_;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	// モデル
+	std::unique_ptr<Model> model_;
+	// プレイヤー
+	std::unique_ptr<Player> player_;
+};
